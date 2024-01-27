3c0221ae8e27f28bf4a892564e826b103ea90a25
Code-XT <rahuljhabokaro123@gmail.com>
2024-01-28 01:10:15 +0530

    added some stuff about me...

--- components/about.tsx
+++ components/about.tsx
@@ -19,16 +19,35 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-        laboriosam necessitatibus animi quas quia dolorum magnam sapiente ad, a
-        amet, nihil sequi perspiciatis in ipsa voluptas voluptatibus reiciendis
-        quibusdam ipsum!
+        Ever since I held the first android phone in my hand in 2012{" "}
+        <span className="font-medium">
+          I understood where my interest lies.
+        </span>
+        <span className="font-medium">
+          This interest only grew the more I learned about science and tech.
+        </span>
+        It was when I first coded something in C++ that I finally decided to
+        pursue my passion for programming, realising the power that I have in my
+        hand, to create anything I want.{" "}
+        <span className="italic">My favorite part of programming</span> is to
+        build new stuff and learn new ways of doing things. My core web stack is{" "}
+        <span className="font-medium italic">
+          React, Next.js, Node.js, and MongoDB
+        </span>
+        . I am also familiar with TypeScript and Tailwind. But, my{" "}
+        <span className="underline">favourit language</span> to code in, is C++
+        (I love the highest level control that it provieds over your program and
+        the system...and no other language even comes close [yeah, not even
+        Rust]) I am always looking to learn new technologies. I am currently
+        looking for a <span className="font-medium">full-time position</span> as
+        a software developer.
       </p>
 
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis odio
-        mollitia aut eveniet aliquam sed corrupti dolor laborum ipsam! Tempore
-        non cum esse nostrum facere! Optio, minus vitae. Iusto, consectetur.
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games, watching anime, and reading manga/vn. I also enjoy{" "}
+        <span className="font-medium">learning new skills</span>. I am currently
+        trying to learn how to play guitar and ride a skateboard.
       </p>
     </motion.section>
   );
