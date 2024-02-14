aa1bbbf46fed544b81b2f0b2bd660640e4b72ede
Code-XT <rahuljhabokaro123@gmail.com>
2024-02-15 00:40:58 +0530

    about changes

--- components/about.tsx
+++ components/about.tsx
@@ -19,35 +19,23 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        Ever since I held the first android phone in my hand in 2012{" "}
-        <span className="font-medium">
-          I understood where my interest lies.
-        </span>
-        <span className="font-medium">
-          This interest only grew the more I learned about science and tech.
-        </span>
-        It was when I first coded something in C++ that I finally decided to
-        pursue my passion for programming, realising the power that I have in my
-        hand, to create anything I want.{" "}
-        <span className="italic">My favorite part of programming</span> is to
-        build new stuff and learn new ways of doing things. My core web stack is{" "}
-        <span className="font-medium italic">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Tailwind. But, my{" "}
-        <span className="underline">favourit language</span> to code in, is C++
-        (I love the highest level control that it provieds over your program and
-        the system...and no other language even comes close [yeah, not even
-        Rust]) I am always looking to learn new technologies. I am currently
-        looking for a <span className="font-medium">full-time position</span> as
-        a software developer.
+        I have been fascinated by technology ever since I got my first Android
+        phone in 2012. Since then, I have been exploring various aspects of
+        science and tech, especially programming. I fell in love with C++ as it
+        gave me the ability to create anything I could imagine. I like to
+        challenge myself with new projects and learn new skills. I am
+        well-versed in web development using the MERN stack, TypeScript, and
+        Tailwind CSS. However, my passion lies in system development and
+        hardware/system architecture, which is why C++ is my favorite language
+        [with Rust being a close second]. It allows me to have full control over
+        the systems I work with. I am always curious about new technologies and
+        seeking full-time opportunities as a software developer.
       </p>
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching anime, and reading manga/vn. I also enjoy{" "}
-        <span className="font-medium italics">learning new skills</span>. I am
-        currently trying to learn how to play guitar and ride a skateboard.
+        video games, watching anime, and reading manga/vn. I'm also passionate
+        about acquiring new skills, currently tackling guitar and skateboarding.
       </p>
     </motion.section>
   );
--- components/intro.tsx
+++ components/intro.tsx
@@ -62,12 +62,13 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Rahul.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span>. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js) & C++</span>. Think of me as
-        an oddball who likes System Programming and learning about core working
-        of systems a lot more than WebDev.
+        <span className="font-bold">Hello, I'm Rahul,</span> a{" "}
+        <span className="font-bold">full-stack developer</span> who enjoys
+        building{" "}
+        <span className="italic">
+          sites & apps that I find fun or interesting
+        </span>
+        . My focus is <span className="underline">React (Next JS) & C++</span>.
       </motion.h1>
 
       <motion.div
