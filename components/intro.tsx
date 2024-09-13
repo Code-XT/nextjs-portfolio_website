d2bf307cbd8d79c1a9f134797b9ceb4ae8669543
Code-XT <rahuljhabokaro123@gmail.com>
2024-09-13 11:41:28 +0530

    resume update

--- components/intro.tsx
+++ components/intro.tsx
@@ -93,7 +93,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Rahul_s_Resume.pdf"
+          href="/Rahul_Resume-final.pdf"
           download
         >
           Download CV{" "}
